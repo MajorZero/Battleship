0e9d45aecb79a01a600242c798fd974ad26a8a37
Dorian <dorian.trevisan@gmail.com>
2014-08-05 13:20:54 +1000

    Main class
    At the moment is doing what Game class will do: create the players and managing the entire game.

---
+++ main.m
@@ -0,0 +1,16 @@
+
+#import <Foundation/Foundation.h>
+#import "Player.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        Player *player1=[[Player alloc]initWithName:@"Player1"];
+        Player *player2=[[Player alloc]initWithName:@"Player2"];
+        
+        
+            }
+    return 0;
+}
